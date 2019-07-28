8f444f63db265adfe46fdcd533b54fe434c3c7a7
dtoolz <donaldaudriel@gmail.com>
2019-07-28 15:33:57 +0100

    added styling to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import dlogo from '../dlogo.PNG';
+import styled from 'styled-components';
+import {ButtonContainer} from "./Button";
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>navbar showing</h3>
-            </div>
-        )
+            <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5" >
+
+               <Link to='/'>
+                 <img src={dlogo} alt="logo" className="navbar-brand" />
+               </Link>
+
+               <ul className="navbar-nav align-items-center"> 
+                 <li className="navbar-item ml-5">
+                  <Link to="/" className="nav-link">
+                   OUR CARS
+                  </Link>
+                 </li>
+               </ul>
+
+               <Link to='/cart' className="ml-auto">
+                 <ButtonContainer>
+                   <span className="mr-2">
+                     <i className="fas fa-cart-plus" />
+                   </span>
+                    my cart
+                 </ButtonContainer>
+               </Link>
+            </NavWrapper>
+        );
     }
 }
+
+
+const NavWrapper= styled.nav`
+background: var(--mainGreen);
+ .nav-link{
+   color: var(--mainWhite) !important;
+   font-size: 1.3rem;
+   text-transform: capitalize;
+ }
+` 
---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+    {
+      id: 1,
+      title: "Tecno Camon CX - Black",
+      img: "img/product-1.png",
+      price: 17,
+      company: "TECNO",
+      info: 
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus  id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut etvoluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+        id: 2,
+        title: "HTC 10 - Black",
+        img: "img/product-2.png",
+        price: 16,
+        company: "HTC",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 3,
+        title: "Google Pixel - Black",
+        img: "img/product-3.png",
+        price: 16,
+        company: "GOOGLE",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 4,
+        title: "iphone 5 plus - white",
+        img: "img/product-4.png",
+        price: 18,
+        company: "APPLE",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 5,
+        title: "Samsung Galaxy S7 - Gold",
+        img: "img/product-5.png",
+        price: 19,
+        company: "SAMSUNG",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 6,
+        title: "Samsung Galaxy S9 - black",
+        img: "img/product-6.png",
+        price: 14,
+        company: "SAMSUNG",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 7,
+        title: "Huawei Mate - Grey",
+        img: "img/product-7.png",
+        price: 17,
+        company: "HUAWEI",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 8,
+        title: "iphone 6 plus - white",
+        img: "img/product-8.png",
+        price: 16,
+        company: "APPLE",
+        info: 
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 16,
+  company: "GOOGLE",
+  info: 
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat." ,
+  inCart: false,
+  count: 0,
+  total: 0
+}
