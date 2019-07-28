71e1da175f1827d2e0cff21b4c01e18af6a80c3b
dtoolz <donaldaudriel@gmail.com>
2019-07-28 11:33:07 +0100

    added react router and linked all components to routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,10 +14,12 @@ class App extends Component {
     return (
      <React.Fragment>
        <Navbar/>
-       <ProductList/>
-       <Details/>
-       <Cart/>
-       <Default/>
+       <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route  component={Default} />
+      </Switch>
      </React.Fragment>
     );
   }
