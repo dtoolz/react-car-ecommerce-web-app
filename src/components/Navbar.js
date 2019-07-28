c0842ac685d8ebec1e5e9474b8ffc9f563a74d1c
dtoolz <donaldaudriel@gmail.com>
2019-07-28 11:01:03 +0100

    added components and linked them to appjs

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>navbar showing</h3>
+            </div>
+        )
+    }
+}
---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>product list showing</h3>
+            </div>
+        )
+    }
+}
