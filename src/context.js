4164bcb0ade8f802d32edff78a4d2e07eb576f34
dtoolz <donaldaudriel@gmail.com>
2019-08-01 10:02:56 +0100

    added functionality for clear cart button and remove item button in the cart

--- src/context.js
+++ src/context.js
@@ -83,11 +83,32 @@ class ProductProvider extends Component {
     };
 
     removeItem = (id) => {
-      console.log('remove');
+      let tempProducts = [...this.state.products];
+      let tempCart = [...this.state.cart];
+   
+      tempCart = tempCart.filter(item => item.id !==id);
+      const index = tempProducts.indexOf (this.getItem(id));
+      let removedProduct = tempProducts[index];
+      removedProduct.inCart = false;
+      removedProduct.count = 0;
+      removedProduct.total = 0;
+   
+      this.setState(() => {
+        return {
+          cart:[...tempCart],
+          products:[...tempProducts]
+        }
+      },()=>{
+         this.addTotals();
+      });
     };
 
     clearCart = () => {
-      console.log('cart cleared');
+      this.setState(()=> {
+          return {cart:[] };
+       }, ()=> {
+        this.setProducts(); this.addTotals();
+      });
     };
 
      addTotals = () => {
